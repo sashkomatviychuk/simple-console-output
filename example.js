5a7ff961da3c9fe2db2fc632952801eb52b65037
Oleksandr Matviichuk <sashko1921@gmail.com>
2018-04-22 00:58:59 +0200

    Updated example

--- example.js
+++ example.js
@@ -1,8 +1,8 @@
 const _console = require('./index');
 
 _console.time('time1');
-_console.log('Im here!', 'Its fine');
+_console.log('Im here!');
 _console.log();
 _console.error('Its error message');
-_console.info('Message with argument %s', 'its_arg_value', 'and its not an argument');
+_console.info('Message with argument %s', 'its_arg_value');
 _console.timeEnd('time1');
