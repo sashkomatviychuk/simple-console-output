0999de57a61f89f948eb02676b86e6ea0c637eeb
Oleksandr Matviichuk <sashko1921@gmail.com>
2018-04-22 00:12:25 +0200

    Add files

---
+++ example.js
@@ -0,0 +1,6 @@
+const _console = require('./index');
+
+_console.log('Im here!', 'Its fine');
+_console.log();
+_console.error('Its error message');
+_console.info('Message with argument %s', 'its_arg_value', 'and its not an argument');
